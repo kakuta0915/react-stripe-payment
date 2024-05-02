bd1b245fe4c6203ab717d6247c759db1e402c428
Takeru Kakuta <kakuta19960915@gmail.com>
2024-05-02 16:39:27 +0900

    サーバー処理

--- server.js
+++ server.js
@@ -1,5 +1,96 @@
-const express  = require("express");
+const stripe = require('stripe')('sk_test_51OwJ4iKhKNkDMmE5r76y79tcC6ZRlb3xiWVBK25mC67XaRieiskJaZb0gdpqOm2xSj7FzeTGlMTt1VJxZrLX9cAS00RE3jMvzE');
+const express = require("express");
 const app = express();
-const PORT = 3000;
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.listen(PORT, console.log("サーバーが起動しました!"));
+const YOUR_DOMAIN = "http://localhost:3000";
+
+app.post("/create-checkout-session", async (req, res) => {
+  try {
+    const prices = await stripe.prices.list({});
+    const session = await stripe.checkout.sessions.create({
+      billing_address_collection: "auto",
+      line_items: [
+        {
+          price: prices.data[0].id,
+          quantity: 1,
+        },
+      ],
+      mode: "subscription",
+      success_url: `${YOUR_DOMAIN}/?success=true&session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+    });
+
+    res.redirect(303, session.url);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.post("/create-portal-session", async (req, res) => {
+  const { session_id } = req.body;
+  const checkoutSession = await stripe.checkout.sessions.retrieve(session_id);
+  const returnUrl = YOUR_DOMAIN;
+
+  const portalSession = await stripe.billingPortal.sessions.create({
+    customer: checkoutSession.customer,
+    return_url: returnUrl,
+  });
+
+  console.log(portalSession.url);
+  res.redirect(303, portalSession.url);
+});
+
+app.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  (request, response) => {
+    const event = request.body;
+
+    const endpointSecret = "whsec_12345";
+    if (endpointSecret) {
+      const signature = request.headers["stripe-signature"];
+      try {
+        event = stripe.webhooks.constructEvent(
+          request.body,
+          signature,
+          endpointSecret
+        );
+      } catch (err) {
+        console.log(`⚠️  Webhook signature verification failed.`, err.message);
+        return response.sendStatus(400);
+      }
+    }
+    let subscription;
+    let status;
+    switch (event.type) {
+      case "customer.subscription.trial_will_end":
+        subscription = event.data.object;
+        status = subscription.status;
+        console.log(`Subscription status is ${status}.`);
+        break;
+      case "customer.subscription.deleted":
+        subscription = event.data.object;
+        status = subscription.status;
+        console.log(`Subscription status is ${status}.`);
+        break;
+      case "customer.subscription.created":
+        subscription = event.data.object;
+        status = subscription.status;
+        console.log(`Subscription status is ${status}.`);
+        break;
+      case "customer.subscription.updated":
+        subscription = event.data.object;
+        status = subscription.status;
+        console.log(`Subscription status is ${status}.`);
+        break;
+      default:
+        console.log(`Unhandled event type ${event.type}.`);
+    }
+    response.send();
+  }
+);
+
+app.listen(3000, () => console.log("Running on port 3000"));
